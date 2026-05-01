3555a73a7fb633891f79ef33020e69d979d59fda
zerobruhman <onebruhmanpc@gmail.com>
2026-05-01 13:56:48 +0700

    Revert "tambah kan read! masih (acak-acak)"
    This reverts commit 180ef743397f1f4347655e8ce4ca785e6aa18cb3.

--- README.md
+++ README.md
@@ -17,20 +17,10 @@ nama VARCHAR(100),
 umur INT
 );
 ```
-Setup ``koneksi.php``
-```php
-$host = "host_yang_kamu_pakai!";
-$user = "username_kamu!";
-$password = "password_kamu!";
-$nama_db = "nama_db_kamu!";
 
-$koneksi = mysqli_connect($host, $user, $password, $nama_db);
-
-if (!$koneksi){
-    die("Koneksi Gagal! : " . mysqli_connect_error());
-}
-```
 ## Next
 -  Menambahkan **Read, Update dan Delete**
 -  Security SQL basic
 
+
+README.md nya rapih yah hahaha
--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 include "koneksi.php";
 
 $hasil = "";
-$hasil_read = "";
-$baris = "";
-
 function create($namatemp, $umurtemp, $koneksitemp){
     global $hasil;
     $trimmednama = trim($namatemp);
@@ -20,39 +17,17 @@ function create($namatemp, $umurtemp, $koneksitemp){
     mysqli_query($koneksitemp, "INSERT INTO CRUD_S (nama, umur) VALUES ('$trimmednama', '$umurbersih')");
     $hasil = "Create berhasil!";
 }
-function read($id, $koneksitemp){
-    global $hasil_read;
-    global $baris;
-    if ($id === "")
-        return $hasil_read = "Id tolong di isi";
-    if (!is_numeric($id))
-        return $hasil_read = "Id harus angka isi";
-
-    $baris = mysqli_fetch_assoc(mysqli_query($koneksitemp, "SELECT * FROM CRUD_S WHERE id = $id"));
-}
-if (isset($_POST['create'])){   
+if (isset($_POST['kirim'])){   
     $nama = $_POST['nama'] ?? "";
     $umur = $_POST['umur'] ?? "";
 
     create($nama, $umur, $koneksi);
-}
-if (isset($_POST['id'])){
-    $id = $_POST['id'] ?? "";
-
-    read($id, $koneksi);
 }
 ?>
 
 <form method="POST">
     Nama : <input name="nama" type="text"><br>
     Umur : <input name="umur" type="number"><br>
-    <button name="create">Buat</button>
+    <button name="kirim">Buat</button>
     <b><?php echo htmlspecialchars($hasil);?></b>
-</form>
-<form method="POST">
-    <h1>Masukkan id untuk Read</h1>
-    Id : <input name="id" type="number">
-    <?php while ($baris){
-        #echo $baris["nama"] . " " . $baris["umur"];
-    }?>
 </form>
